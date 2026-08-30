85d10c59eb1df1829c63dba942508cf8984fa2c7
RomanSame <sobakapes322@gmail.com>
2026-08-30 15:41:26 +0500

    7 page of tourgo

--- main.go
+++ main.go
@@ -5,7 +5,23 @@ import (
 	"math/rand"
 )
 
+func add(x, y int) int {
+	return x + y
+}
+func swap(x, y string) (string, string) {
+	return y, x
+}
 func main() {
-	fmt.Println("eto main nahuy")
-	fmt.Println(" my dick is " + fmt.Sprint(rand.Intn(100)) + " cantimeters long")
+	fmt.Print("watsup!")                                                                       // bez perenosa
+	fmt.Println("eto main nahuy")                                                              // s perenosom
+	fmt.Println("my dick is " + fmt.Sprint(rand.Intn(30)) + " cantimeters long")               //combo no sprint poka hz che eto
+	fmt.Printf("nickname: %s from zero to ten - %v, ghoul: %v - 7\n", "samerism", 9.832, 1000) // formatirovanni
+
+	//%s - stroka %d - celoe chislo %f - drobnoe %v - universal absolute typeshi %T - pokazat
+
+	sdelal := 1
+	fmt.Println(sdelal)
+	fmt.Println(add(1337, 1448))
+	a, b := swap("swop", "swip")
+	fmt.Println(a, b)
 }
